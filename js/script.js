5e718dcfbcf98c6fbf2cae4b6c3a21a69e4d7e39
prayismail <yogamailforalvin@gmail.com>
2026-05-15 05:45:32 +0800

    script.js

--- js/script.js
+++ js/script.js
@@ -2692,7 +2692,7 @@ function showVaaNotificationOnMap(vaaData) {
     const seqInput = document.createElement('input');
     seqInput.type = 'text';
     seqInput.className = 'sigmet-seq-input';
-    seqInput.placeholder = 'Nomor(cth:01)';
+    seqInput.placeholder = 'Isi nomor SIGMET (cth: 01)';
     seqInput.maxLength = 2;
     buttonContainer.appendChild(seqInput); // Ditambahkan ke container
     // -----------------------------------------------------
